8ea315eff243b82eb37de3d4e68e27222f40dc72
bozo690 <139140501+bozo690@users.noreply.github.com>
2025-08-27 19:45:41 +0530

    39167

--- home/views.py
+++ home/views.py
@@ -2,35 +2,20 @@ import requests
 from django.shortcuts import render
 from django.conf import settings
 
-def home(request)
+def menu_page(request)
     """
     View function for the homepage.
     This function fetches menu data from your API and passes it to the template.
     """
 
-    api_url='http://127.0.0.1:8000/api/menu/'
+    all_items=Item.objects.all()
 
-    menu_data=[]
-    error_message=None
-    
-    try:
-        response=requests.get(api_url)
-
-        response.raise_for_status()
-
-        menu_data=response.json()
-
-    except requests.exceptions.RequestException as e:
-        error_message=f"Could not connect to make API. Please try again later. Error:{e}"
-    except ValueError:
-        error_message="Could not parse the response from the menu API."
-    
     context={
-        'menu_items':menu_data,
-        'error_message':error_message,
+        'menu_items': all_items,
         'restaurant_name': settings.RESTAURANT_NAME,
     }
 
-    return render(request, 'home/index.html', context)
+
+    return render(request, 'home/menu.html', context)   
 
 
