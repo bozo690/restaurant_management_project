e842817b2d27b0f1102802a87b8c4f1c8978a79c
bozo690 <139140501+bozo690@users.noreply.github.com>
2025-08-27 20:10:27 +0530

    39164

--- home/models.py
+++ home/models.py
@@ -1,29 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
-class Profile(models.Model):
-    """
-    Extends the built-in django user model to include additional user information.
-    """
+   class ContactSubmission(models.Model):
+        """
+        Represents a single submission from the contact form.
+        """
+        mame=models.CharField(max_length=100)
+        email=models.EmailField()
+        message=models.TextField()
+        created_at=models.DateTimeField(auto_now_add=True)
+        
 
-    user=models.OneToOneField(User, on_delete=models.CASCADE)
-
-
-    mame=models.CharField(max_length=100, blank=True)
-    email=models.EmailField(max_length=150, blank=True)
-    phone_numer=models.CharField(max_length=20, blank=True)
-
-    def __str__(self):
-        return self.user.username
+        def __str__(self):
+            return f"Message from{self.name} on  {self.created_at.strftime('%Y-%Ym-%d')} "
 
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    """
-    Signal handler to create a profile instance when a new user is created.
-    """
-    instance.profile.save()
 
-# Create your models here.
+
+
